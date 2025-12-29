2bd8d0d9cb9901ca5539b33efd8549d37c0d5449
A2N1 <155444561+A2N1@users.noreply.github.com>
2025-12-29 20:41:08 +0100

    add comments to 01Introduction.sql for clarity

--- SQLCourse/01Introduction.sql
+++ SQLCourse/01Introduction.sql
@@ -1,8 +1,11 @@
-What is SQL?
+--What is SQL?
+--Structure Query Language (SQL) is a standard programming language specifically designed for managing and manipulating relational databases. 
+--It allows users to create, read, update, and delete data within a database.
+--pronounced as "sequel" or by its initials "S-Q-L".
 
-Why learn SQL?
+--Why learn SQL?
 
-What is Database & Types?
+--What is Database & Types?
 
 SQL Commands
 
