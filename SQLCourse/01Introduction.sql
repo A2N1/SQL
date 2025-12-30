91b815686921f248eb754aaa574d3e739f399c3d
A2N1 <155444561+A2N1@users.noreply.github.com>
2025-12-30 23:45:36 +0100

    enhance database types section and add database structure details in 01Introduction.sql

--- SQLCourse/01Introduction.sql
+++ SQLCourse/01Introduction.sql
@@ -26,15 +26,25 @@
 --Server :: Where Databse Lives
 
 --Database Types:
---Relational Databases (RDBMS): Store data in tables with rows and columns. Examples: MySQL, PostgreSQL, Oracle, SQL Server.
+--Relational Databases (RDBMS): Store data in tables with rows and columns. Examples: MySQL, PostgreSQL, Oracle, Microsoft SQL Server.
 --SQL is primarily used with relational databases.
 --No SQL Databases: Designed for unstructured data and flexible schemas. Examples: MongoDB, Cassandra, Redis.
 --Types of No SQL Databases:
---Key-Value Stores: Store data as key-value pairs. Examples: Redis, DynamoDB.
---Column-Based Databases: Store data in columns rather than rows. Examples: Apache Cassandra, HBase.
+--Key-Value Stores: Store data as key-value pairs. Examples: Redis, Amazon DynamoDB.
+--Column-Based Databases: Store data in columns rather than rows. Examples: Apache Cassandra, HBase, Amazon Redshift.
 --Graph Databases: Store data in graph structures with nodes and edges. Examples: Neo4j, Amazon Neptune.
 --Document-Based Databases: Store data as documents, often in JSON or XML format. Examples: MongoDB, CouchDB.
 
+--Databse Structure:
+--Server can host multiple Databases
+--Databeses can have multiple Schemas (Tables)
+--A table is made up of rows and columns with each column representing a specific attribute of the data and each row representing a single record.
+--Primary Key: A unique identifier for each record in a table.
+--Value: The actual data stored in the table's cells.
+--Data Types: Define the type of data that can be stored in each column (e.g., INTEGER, VARCHAR, DATE).
+--Numerie: Integer (1, 2, 30), Float, Decimal (3.14, 100,50)
+--Text as Strings: Char, Varchar, Text ('Maria', 'Hello World')
+--Date and Time: Date ('2025-10-30'), Time ('09:30:00'), Timestamp
 
 SQL Commands
 
